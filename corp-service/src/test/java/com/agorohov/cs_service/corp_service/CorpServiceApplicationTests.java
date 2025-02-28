a5e2d4d36db12d01377dd8a35f69aa17392f392c
agorohov <aleksandr.gorohov@gmail.com>
2025-02-28 21:14:19 +0300

    init commit

---
+++ corp-service/src/test/java/com/agorohov/cs_service/corp_service/CorpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.cs_service.corp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
